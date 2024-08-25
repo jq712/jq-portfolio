cc0381b746a131201f2f6d438b6f05227c0bcac1
Jordan Quarles <jq@Jordans-Air.lan>
2024-08-25 01:24:00 -0400

    Add dark mode preference feature

--- public/scripts.js
+++ public/scripts.js
@@ -12,3 +12,22 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('navbar-scrolled');
   }
 });
+
+if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+}
+
+
+let themeSwitch = document.getElementById('checkbox');
+
+themeSwitch.addEventListener('change', function() {
+    if (this.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+});
+themeSwitch.addEventListener('change', function() {
+    localStorage.setItem('theme', this.checked ? 'dark' : 'light');
+});
+
